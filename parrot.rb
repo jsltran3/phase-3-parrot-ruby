b5071e3e8f9c6fe0a6eb6ac44089a9336b0ad648
Joseph <jsltran@gmail.com>
2022-09-01 17:25:48 -0700

    completed

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,20 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+require 'pry'
+# The `parrot` method should accept an argument of a string and both `puts` out
+# that string _and_ return the string at the end of the method.
+
+# The `parrot` method should have a default argument of `"Squawk!"`.
+
+def parrot(bird = "Squawk!")
+    puts bird
+    return bird
+  
+
+end 
+
+#do this step by step instead of all at once
+
+# graveyard
+# puts bird unless str.empty?
+# bird
+
+#REUTRNS ARE IMPLICITR BREAKS. Nothing else happens after "return". GOOGLE THIS
